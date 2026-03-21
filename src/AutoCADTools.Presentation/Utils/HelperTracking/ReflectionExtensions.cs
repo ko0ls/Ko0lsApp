83ba6de050cb5f98bfdcdb4376e0046f7d94b394
Ko0ls <phuongnamlc92@gmail.com>
2026-03-21 13:52:43 +0700

    feat: add trackable base

---
+++ src/AutoCADTools.Presentation/Utils/HelperTracking/ReflectionExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace AutoCADTools.Presentation.Utils.HelperTracking
+{
+    public static class ReflectionExtensions
+    {
+        public static bool IsNullable(this Type type, out Type? underlyingType)
+        {
+            underlyingType = Nullable.GetUnderlyingType(type);
+            return underlyingType != null;
+        }
+
+        public static bool IsNullable(this Type type)
+        {
+            var underlyingType = Nullable.GetUnderlyingType(type);
+            return underlyingType != null;
+        }
+    }
+}
---
+++ src/AutoCADTools.Presentation/Utils/ObjectCloner.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AutoCADTools.Presentation.Utils
+{
+    public static class ObjectCloner
+    {
+        public static object? Clone(this object? source)
+        {
+            if (source == null) return null!;
+
+            using var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, source);
+            stream.Position = 0;
+            return formatter.Deserialize(stream)!;
+        }
+    }
+}
