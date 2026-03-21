b84200aa16aac072ff91b5aa2202b25a4872c815
Ko0ls <phuongnamlc92@gmail.com>
2026-03-21 14:05:26 +0700

    refactor: create partial class

---
+++ src/AutoCADTools.Presentation/Utils/BindableObject.Data.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using AutoCADTools.Presentation.Utils.HelperTracking;
+using Newtonsoft.Json;
+
+namespace AutoCADTools.Presentation.Utils;
+
+public abstract partial class BindableObject
+{
+    #region Data
+
+    protected Dictionary<string, ITrackedChange> TrackedDictionary { get; set; }
+    private bool _isChanged;
+    private bool _isValidModel;
+    private CancellationTokenSource? _debounceCts;
+    private int _debounceTime = 50;
+    private bool _isAnyPropsDirty;
+
+    [JsonIgnore]
+    public bool IsChanged
+    {
+        get
+        {
+            if (!IsTracking) return _isChanged;
+            var trackedChanges = UpdateTracked();
+            _isChanged = trackedChanges.Where(x => x.Property.Name != string.Empty).Any(x => x.DetectChange(GetValue(x.Property)));
+            return _isChanged;
+        }
+        private set => _isChanged = value;
+    }
+
+    [JsonIgnore]
+    public bool IsValidModel
+    {
+        get => _isValidModel;
+        set => SetProperty(ref _isValidModel, value);
+    }
+
+    private bool IsTracking => TrackedDictionary.Count > 0;
+
+    public bool IsAnyPropsDirty
+    {
+        get => _isAnyPropsDirty;
+        set => SetProperty(ref _isAnyPropsDirty, value);
+    }
+
+    #endregion
+}
