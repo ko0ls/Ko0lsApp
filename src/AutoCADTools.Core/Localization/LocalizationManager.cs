861d5a6a855abf7085787865e620020e68ec23cc
ko0ls <phuongnamlc92@gmail.com>
2026-03-22 17:15:10 +0700

    feat: add resource

---
+++ src/AutoCADTools.Core/Localization/LocalizationManager.cs
@@ -0,0 +1,37 @@
+#nullable enable
+
+using System;
+using System.Globalization;
+using System.Resources;
+using System.Threading;
+
+namespace AutoCADTools.Core.Localization
+{
+  public static class LocalizationManager
+  {
+    private static readonly ResourceManager _resourceManager =
+      new ResourceManager(
+        "AutoCADTools.Core.Localization.Resources.Strings",
+        typeof(LocalizationManager).Assembly);
+
+    private static string _currentCulture = "en";
+
+    public static event EventHandler? LanguageChanged;
+
+    public static string CurrentCulture => _currentCulture;
+
+    public static void SetLanguage(string cultureName)
+    {
+      var culture = new CultureInfo(cultureName);
+      _currentCulture = cultureName;
+      Thread.CurrentThread.CurrentUICulture = culture;
+      Thread.CurrentThread.CurrentCulture = culture;
+      LanguageChanged?.Invoke(null, EventArgs.Empty);
+    }
+
+    public static string GetString( this string key)
+    {
+      return _resourceManager.GetString(key, CultureInfo.CurrentUICulture) ?? key;
+    }
+  }
+}
