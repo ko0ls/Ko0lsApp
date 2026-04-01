7c2daa5056ae1612f26ef19a4f594ff2913d46f1
ko0ls <phuongnamlc92@gmail.com>
2026-04-01 12:03:23 +0700

    [fix] Comment #3 from @ko0ls: remove unused imports
    Resolved: [FYI] comment
    
    Reviewer: ko0ls
    
    File: src/AutoCADTools.App/Foundation/SwallowFoundationDrawer.cs:1-12
    
    PR: #13

--- src/AutoCADTools.App/Foundation/SwallowFoundationDrawer.cs
+++ src/AutoCADTools.App/Foundation/SwallowFoundationDrawer.cs
@@ -1,14 +1,9 @@
 #nullable enable
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
-using Autodesk.AutoCAD.Runtime;
 using AutoCADTools.Core;
 
 namespace AutoCADTools.App.Foundation;
